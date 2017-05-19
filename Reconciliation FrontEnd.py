0eb226442ab86b5858aac27ea9c36ffa3d93eaf9
Daniel White <daniel.white@foundlifestyle.co.uk>
2017-05-19 15:12:53 +0100

    Added Error Message for not selecting Sales or Stock
    Error message when sales or stock is not selected in GUI

--- Reconciliation FrontEnd.py
+++ Reconciliation FrontEnd.py
@@ -1,14 +1,20 @@
-from tkinter import*
+from tkinter import *
+from tkinter import messagebox
 from tkinter.filedialog import askopenfilename
 
-#Sets Tkinter
-root = Tk()
-
 def selectFiles():
-    root.withdraw
-    #User inputs for location of files and whether Sales or Stock for reconciliation
-    BOReport = askopenfilename(parent=root,initialdir="/",title='Please select a BO Report')
-    INCReport = askopenfilename(parent=root,initialdir="/",title='Please select an INC Report')
+    Sales2 = Sales.get()
+    Stock2 = Stock.get()
+    if Sales2 == 0 and Stock2 == 0:
+        messagebox.showwarning("Error", "You need to select sales or stock")
+    else:
+        root.withdraw
+        #User inputs for location of files and whether Sales or Stock for reconciliation
+        BOReport = askopenfilename(parent=root,initialdir="/",title='Please select a BO Report')
+        INCReport = askopenfilename(parent=root,initialdir="/",title='Please select an INC Report')
+
+#Sets Tkinter root window
+root = Tk()
 
 Label (root, text = "Sales or Stock").grid(row=0, sticky=W)
 Sales = IntVar()
@@ -18,9 +24,3 @@ Checkbutton(root, text="Stock", variable = Stock).grid(row=2, sticky=W)
 Button(root, text = 'Quit', command=root.quit).grid(row=4, sticky=W, pady=4)
 Button(root, text = 'Next', command = selectFiles).grid(row=3, sticky=W, pady=4)
 mainloop()
-
-
-
-
-
-    
