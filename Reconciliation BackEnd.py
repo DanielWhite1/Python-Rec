a8da6613647a9ccabc2235aae1e3f072ae854bc2
Daniel White <daniel.white@foundlifestyle.co.uk>
2017-06-01 16:15:17 +0100

    Access to Sales and Stock Rec
    Updated to fix issues regarding loading files into sales and stock
    reconciliation

--- Reconciliation BackEnd.py
+++ Reconciliation BackEnd.py
@@ -1,2 +1,22 @@
 import openpyxl
+import FrontEnd
 
+#Checks sales or stock based on the GUI
+if FrontEnd.Sales == 1:
+    Sales(FrontEnd.BOReport, FrontEnd.INCReport)
+else:
+    Stock(FrontEnd.BOReport, FrontEnd.INCReport)
+
+#Runs Sales Reconciliation
+def Sales(BOReport, INCReport):
+    #Sets Excel files to reports
+    BO = Workbook(BOReport)
+    INC = Workbook(INCReport)
+    print("See me Sales")
+
+#Runs Stock Reconciliation
+def Stock(BOReport, INCReport):
+    #Sets Excel files to reports
+    BO = Workbook(BOReport)
+    INC = Workbook(INCReport)
+    print("Watch me stock, stock")
--- Reconciliation FrontEnd.py
+++ Reconciliation FrontEnd.py
@@ -1,3 +1,5 @@
+import openpyxl
+from openpyxl.reader.excel import load_workbook
 from tkinter import *
 from tkinter import messagebox
 from tkinter.filedialog import askopenfilename
@@ -12,6 +14,32 @@ def selectFiles():
         #User inputs for location of files and whether Sales or Stock for reconciliation
         BOReport = askopenfilename(parent=root,initialdir="/",title='Please select a BO Report')
         INCReport = askopenfilename(parent=root,initialdir="/",title='Please select an INC Report')
+        Reconciliation(Sales2,BOReport, INCReport)
+
+def Reconciliation(Sales,BOReport, INCReport):
+
+    #Runs Sales Reconciliation
+    def SalesRec(BOReport, INCReport):
+        selectFiles
+        #Sets Excel files to reports
+        BO = load_workbook(BOReport)
+        INC = load_workbook(INCReport)
+        print("See me Sales")
+
+    #Runs Stock Reconciliation
+    def StockRec(BOReport, INCReport):
+        selectFiles
+        #Sets Excel files to reports
+        print(BOReport)
+        print(INCReport)
+        BO = load_workbook(BOReport)
+        INC = load_workbook(INCReport)
+        print("Watch me stock, stock")
+
+    if Sales == 1:
+        SalesRec(BOReport, INCReport)
+    else:
+        StockRec(BOReport, INCReport)
 
 #Sets Tkinter root window
 root = Tk()
@@ -24,3 +52,4 @@ Checkbutton(root, text="Stock", variable = Stock).grid(row=2, sticky=W)
 Button(root, text = 'Quit', command=root.quit).grid(row=4, sticky=W, pady=4)
 Button(root, text = 'Next', command = selectFiles).grid(row=3, sticky=W, pady=4)
 mainloop()
+
